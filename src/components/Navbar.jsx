6f2e8ed538ebfc6796285ae7f06853241a2ec6b6
Jan Manikowski <janek210102j@gmail.com>
2024-12-03 18:33:58 +0100

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+// src/components/Navbar.js
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>Discarded Band</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Music</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
